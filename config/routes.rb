73bef883673b53f7e3f44829128feb5acfc2e9a0
Kawase-Kota <kottan00@gmail.com>
2021-06-07 22:21:20 +0900

    サンプルアプリの多言語化（英語、日本語対応）を行いました。

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
   resources :books
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  # Pathに対し言語設定が含まれるようにする
+  scope "/:locale" do
+    resources :books
+  end
 end
+
