6e8f95351bb27b7ffe04a30469c228893fb2938c
Kawase-Kota <kottan00@gmail.com>
2021-06-12 00:14:01 +0900

    Not Null制約を後から変更しました

--- db/migrate/20210610114713_add_columns_to_users.rb
+++ db/migrate/20210610114713_add_columns_to_users.rb
@@ -1,8 +1,8 @@
 class AddColumnsToUsers < ActiveRecord::Migration[6.1]
   def change
-    add_column :users, :name, :string
-    add_column :users, :postal_code, :string
-    add_column :users, :address, :text
-    add_column :users, :description, :text
+    add_column :users, :name, :string, null: false
+    add_column :users, :postal_code, :string, null: false
+    add_column :users, :address, :text, null: false
+    add_column :users, :description, :text, null: false
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -29,10 +29,10 @@ ActiveRecord::Schema.define(version: 2021_06_10_114713) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.string "name"
-    t.string "postal_code"
-    t.text "address"
-    t.text "description"
+    t.string "name", null: false
+    t.string "postal_code", null: false
+    t.text "address", null: false
+    t.text "description", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
