11193b8eb3d1a574f15818cb20a572fdfe25bb3d
Kawase-Kota <kottan00@gmail.com>
2021-06-12 17:39:37 +0900

    Userコントローラーの作成、ユーザ詳細画面の実装をおこないました

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+    registrations: 'users/registrations'
+  }
+
+  resources :users, only: [:show]
   resources :books
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   root to: "books#index"
 end
