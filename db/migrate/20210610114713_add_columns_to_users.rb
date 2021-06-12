9e086dce03f2f07435afa84cefabeb71b168c2b3
Kawase-Kota <kottan00@gmail.com>
2021-06-12 12:12:42 +0900

    UserテーブルにDEFAULT制約の設定をしました

--- db/migrate/20210610114713_add_columns_to_users.rb
+++ db/migrate/20210610114713_add_columns_to_users.rb
@@ -1,8 +1,8 @@
 class AddColumnsToUsers < ActiveRecord::Migration[6.1]
   def change
     add_column :users, :name, :string, null: false
-    add_column :users, :postal_code, :string, null: false
-    add_column :users, :address, :text, null: false
-    add_column :users, :description, :text, null: false
+    add_column :users, :postal_code, :string, null: false, default: ""
+    add_column :users, :address, :text, null: false, default: ""
+    add_column :users, :description, :text, null: false, default: ""
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -30,9 +30,9 @@ ActiveRecord::Schema.define(version: 2021_06_10_114713) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "name", null: false
-    t.string "postal_code", null: false
-    t.text "address", null: false
-    t.text "description", null: false
+    t.string "postal_code", default: "", null: false
+    t.text "address", default: "", null: false
+    t.text "description", default: "", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
