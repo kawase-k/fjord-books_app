c031322d0e7bb08713a525efd8ef33c4efa30828
Kawase-Kota <kottan00@gmail.com>
2021-06-13 15:13:27 +0900

    i18n対応の修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,6 +9,6 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
-    devise_parameter_sanitizer.permit(:account_update, keys: %i[name postal_code address description email])
+    devise_parameter_sanitizer.permit(:account_update, keys: %i[name postal_code address description])
   end
 end
