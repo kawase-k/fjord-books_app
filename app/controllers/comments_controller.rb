3398aa5604f98d7a98712ab4b0b28183d3fc9a6b
Kawase-Kota <kottan00@gmail.com>
2021-06-22 00:04:26 +0900

    createアクションを追加しました

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,23 @@
 # frozen_string_literal: true
 
 class CommentsController < ApplicationController
-  def create; end
+  before_action :set_commentable
+
+  def create
+    @comment = @commentable.comments.new(comment_params)
+    @comment.user = current_user
+    @comment.save
+    redirect_to @commentable, notice: t('controllers.common.notice_create', name: Comment.model_name.human)
+  end
+
+  private
+
+  def set_commentable
+    resource, id = request.path.split('/')[1, 2]
+    @commentable = resource.singularize.classify.constantize.find(id)
+  end
+
+  def comment_params
+    params.require(:comment).permit(:body)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,7 @@ Rails.application.routes.draw do
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
   devise_for :users
   root to: 'books#index'
-  resources :books do
-    resources :comments, only: %i[create]
-  end
-  resources :reports do
+  resources :books, :reports do
     resources :comments, only: %i[create]
   end
   resources :users, only: %i[index show] do
