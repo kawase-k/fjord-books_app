1ea4b38c5f24bfcbea35fd04e52d9f43c045e216
Kawase-Kota <kottan00@gmail.com>
2021-07-02 02:37:37 +0900

    テストの追加と文面の変更をしました

--- test/system/reports_test.rb
+++ test/system/reports_test.rb
@@ -21,22 +21,28 @@ class ReportsTest < ApplicationSystemTestCase
     visit reports_url
     click_on '新規作成'
 
-    fill_in '内容', with: @report.content
     fill_in 'タイトル', with: @report.title
+    fill_in '内容', with: @report.content
     click_on '登録する'
 
     assert_text '日報が作成されました'
+
+    assert_equal '初日報', @report.title
+    assert_equal '夜は涼しくて過ごしやすいです。', @report.content
   end
 
   test 'updating a Report' do
     visit reports_url
     click_on '編集'
 
-    fill_in '内容', with: @report.content
-    fill_in 'タイトル', with: @report.title
+    fill_in 'タイトル', with: '2日目です'
+    fill_in '内容', with: 'これからもがんばります。'
     click_on '更新する'
 
     assert_text '日報が更新されました。'
+
+    assert_text '2日目です'
+    assert_text 'これからもがんばります。'
   end
 
   test 'destroying a Report' do
