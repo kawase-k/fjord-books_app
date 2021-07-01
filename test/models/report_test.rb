46a44dac215f87025a35314aa37d81e988d50dac
Kawase-Kota <kottan00@gmail.com>
2021-07-01 20:19:22 +0900

    日報のモデルテストとシステムテストを書きました

--- test/models/report_test.rb
+++ test/models/report_test.rb
@@ -3,7 +3,18 @@
 require 'test_helper'
 
 class ReportTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test '#editable?' do
+    user = User.create!(email: 'user@exeample.com', password: 'password')
+    report = Report.create!(user: user, title: '初日報', content: 'よろしくお願いいたします')
+
+    assert report.editable?(user)
+  end
+
+  test '#created_on' do
+    user = User.create!(email: 'user@exeample.com', password: 'password')
+    report = Report.create!(user: user, title: '初日報', content: 'よろしくお願いいたします')
+
+    assert report.created_on
+    assert_not_equal report, report.created_on
+  end
 end
